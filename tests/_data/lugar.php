f217046dfc70072ed129dd5aa9ee05f3d55122ea
cep11p <cep11p@gmail.com>
2019-01-23 13:30:05 -0300

    Se agregan mas fixtures

--- tests/_data/lugar.php
+++ tests/_data/lugar.php
@@ -61,6 +61,266 @@ return [
         "localidadid"=>2,
         "latitud"=>"-123123",
         "longitud"=>"321123"
+    ],
+    "lugar7" => [
+        "barrio"=>"Rivadavia",
+        "calle"=>"Av. Perón",
+        "altura"=>"444",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar8" => [
+        "barrio"=>"Rivadavia",
+        "calle"=>"Sarmiento",
+        "altura"=>"402",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar9" => [
+        "barrio"=>"Rivadavia",
+        "calle"=>"Sarmiento",
+        "altura"=>"404",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar10" => [
+        "barrio"=>"Rivadavia",
+        "calle"=>"Sarmiento",
+        "altura"=>"504",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar11" => [
+        "barrio"=>"Rivadavia",
+        "calle"=>"Sarmiento",
+        "altura"=>"506",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar12" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Irigoyen",
+        "altura"=>"206",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar13" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Irigoyen",
+        "altura"=>"240",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar14" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"240",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar14" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"2400",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar14" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"406",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar15" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"1406",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar16" => [
+        "barrio"=>"Paterno",
+        "calle"=>"Bernal",
+        "altura"=>"107",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar17" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Bernal",
+        "altura"=>"200",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>2,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar18" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"333",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>1,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar19" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Sarmiento",
+        "altura"=>"3320",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>1,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar20" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Saavedra",
+        "altura"=>"110",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>1,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar21" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Saavedra",
+        "altura"=>"120",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>1,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar22" => [
+        "barrio"=>"Mitre",
+        "calle"=>"Saavedra",
+        "altura"=>"1120",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>1,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar23" => [
+        "barrio"=>"San Martin",
+        "calle"=>"Pringles",
+        "altura"=>"110",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar24" => [
+        "barrio"=>"San Martin",
+        "calle"=>"Pringles",
+        "altura"=>"120",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar25" => [
+        "barrio"=>"San Martin",
+        "calle"=>"Cipoletti",
+        "altura"=>"220",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar26" => [
+        "barrio"=>"San Martin",
+        "calle"=>"Cipoletti",
+        "altura"=>"240",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar27" => [
+        "barrio"=>"San Martin",
+        "calle"=>"Cipoletti",
+        "altura"=>"340",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar28" => [
+        "barrio"=>"America",
+        "calle"=>"Mexico",
+        "altura"=>"340",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar29" => [
+        "barrio"=>"America",
+        "calle"=>"Mexico",
+        "altura"=>"540",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
+    ],
+    "lugar30" => [
+        "barrio"=>"America",
+        "calle"=>"Mexico",
+        "altura"=>"523",
+        "piso"=>"",
+        "depto"=>"",
+        "localidadid"=>3,
+        "latitud"=>"-123123",
+        "longitud"=>"321123"
     ]
     
     
